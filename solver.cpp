01f2e8b78e112dc6bc66aecb4588744759d5b3b1
nERD8932 <56185861+nERD8932@users.noreply.github.com>
2020-02-16 22:48:45 +0530

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int solvegrid[9][9][9],sudo[9][9];//= {9,5,8,2,0,0,7,0,0,0,0,0,9,0,4,6,0,0,0,7,0,8,0,0,0,1,0,0,4,7,0,1,9,0,6,8,0,0,5,0,8,0,0,0,0,0,6,0,0,5,0,0,0,0,5,0,0,0,9,8,0,0,3,0,8,1,0,0,0,9,0,0,0,0,0,6,2,0,0,0,0};
+int solvegrid[9][9][9],sudo[9][9];
 int convert[2][9] = {0,1,2,3,4,5,6,7,8,0,0,0,3,3,3,6,6,6};
 
 struct game
@@ -16,6 +16,7 @@ struct game
     int loopsolve();
     int solve1(); /** NARROW DOWN POSSIBLE NUMBERS **/
     int solve2();/** IF ONLY ONE NUMBER IS POSSIBLE IN AN ELEMENT, ADD IT TO THE SOLUTION **/
+    int checkns(int a, int b);/**NAKED SINGLE TEST**/
     int clearsq(int a, int b, int c);
 };
 
@@ -31,7 +32,7 @@ int game::menu()
 {
     system("cls");
     system("title -SUDOKU SOLVER-");
-    printf("      -SUDOKU SOLVER-\n\n\n 1. Input Sudoku and solve\n 2. Exit");
+    printf("      -SUDOKU SOLVER-\n\n\n 1. Input Sudoku and solve\n 2. Exit\n\n");
     switch(getch())
     {
     case 49:
@@ -68,27 +69,27 @@ int game::inputsudo()
             input = getch();
             if(input==8)
             {
-                if(j==0&&i!=0){i=i-1;j=7;}
-                else if(j!=0){j=j-2;}
+                if(j==0&&i!=0){i=i-1;j=7;sudo[i][j+1]=0;}
+                else if(j!=0){j=j-2;sudo[i][j+1]=0;}
             }
             else if(input<48||input>57&&input!=8){printf("\n\n Incorrect input, returning to menu...(press any key to continue)");getch();obj.menu();}
             else{sudo[i][j] = input - 48;}
             obj.display();
         }
     }
-    printf("\n\n Input successfully taken, press Y to solve sudoku, or, press N to go back to the main menu.");
+    printf("\n\n Input successfully taken,\n press Y to solve sudoku, or, press N to go back to the main menu.");
     switch(getch())
     {
-        case 121: //y
+        case 121: //Y
             obj.loopsolve();
             break;
-        case 89:
+        case 89: //Y
             obj.loopsolve();
             break;
-        case 110:
+        case 110: //N
             obj.menu();
             break;
-        case 78:
+        case 78: //N
             obj.menu();
             break;
         default:
@@ -102,6 +103,7 @@ int game::loopsolve()
     bool flag;
     obj.solve1();
     obj.solve2();
+    for(int i=0;i<7;i=i+3){for(int j=0;j<7;j=j+3){obj.checkns(i,j);}}
     obj.display();
     flag = false;
     for(int i=0;i<9;i++)
@@ -111,7 +113,7 @@ int game::loopsolve()
             if(sudo[i][j]==0){flag = true;}
         }
     }
-    if(flag = true){obj.loopsolve();}
+    if(flag==true){obj.loopsolve();}
     else{printf("\n\nSolved! (press any button to return to main menu)");getch();obj.menu();}
 }
 
@@ -138,6 +140,64 @@ int game::solve1()
     }
 }
 
+int game::checkns(int a,int b)
+{
+    int occ=0,occ2=0;
+    for(int k=0; k<9;k++)
+    {
+        occ=0;
+        for(int i=a;i<a+3;i++)
+        {
+            for(int j=b;j<b+3;j++)
+            {
+                if(solvegrid[k][i][j]==k+1){occ++;}
+            }
+        }
+        if(occ<=3&&occ!=0)
+        {
+            for(int i=a;i<a+3;i++)
+            {
+                occ2=0;
+                for(int j=b;j<b+3;j++)
+                {
+                    if(solvegrid[k][i][j]==k+1){occ2++;}
+                }
+                if(occ2==occ)
+                {
+                    for(int l=0;l<9;l++)
+                    {
+                        if(l<b||l>b+2)
+                        {
+                            solvegrid[k][i][l] = 0;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            for(int i=b;i<b+3;i++)
+            {
+                occ2=0;
+                for(int j=a;j<a+3;j++)
+                {
+                    if(solvegrid[k][j][i]==k+1){occ2++;}
+                }
+                if(occ2==occ)
+                {
+                    for(int l=0;l<9;l++)
+                    {
+                        if(l<a||l>a+2)
+                        {
+                            solvegrid[k][l][i] = 0;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
+
 int game::solve2()
 {
     int occ;
@@ -155,6 +215,18 @@ int game::solve2()
                 for(int k=0;k<9;k++)
                 {
                     if(solvegrid[j][i][k]==j+1){sudo[i][k] = j+1;}
+                }
+            }
+            occ=0;
+            for(int k=0;k<9;k++)
+            {
+                if(solvegrid[j][k][i]==j+1){occ++;}
+            }
+            if(occ==1)
+            {
+                for(int k=0;k<9;k++)
+                {
+                    if(solvegrid[j][k][i]==j+1){sudo[k][i] = j+1;}
                 }
             }
         }
@@ -189,16 +261,16 @@ int game::display()
         else{printf("\n\n");}
     }
     printf(" +-----------------------------+");
-    /*printf("\n\n\n");
+   /* printf("\n\n\n");
     for(int i=0;i<9;i++)
     {
         printf(" ");
         for(int j=0;j<9;j++)
         {
-            printf("%i  ",solvegrid[6][i][j]);
+            printf("%i  ",solvegrid[2][i][j]);
         }
         printf("\n\n");
-    }*/
-    return 0;
+    }
+    return 0;*/
 }
 
