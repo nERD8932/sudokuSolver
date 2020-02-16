dd7398a35b6fb32e9e8346d707b0664be843a968
nERD8932 <56185861+nERD8932@users.noreply.github.com>
2020-02-16 19:34:23 +0530

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
-int solvegrid[9][9][9],sudo[9][9] = {9,5,8,2,0,0,7,0,0,0,0,0,9,0,4,6,0,0,0,7,0,8,0,0,0,1,0,0,4,7,0,1,9,0,6,8,0,0,5,0,8,0,0,0,0,0,6,0,0,5,0,0,0,0,5,0,0,0,9,8,0,0,3,0,8,1,0,0,0,9,0,0,0,0,0,6,2,0,0,0,0};
+int solvegrid[9][9][9],sudo[9][9];//= {9,5,8,2,0,0,7,0,0,0,0,0,9,0,4,6,0,0,0,7,0,8,0,0,0,1,0,0,4,7,0,1,9,0,6,8,0,0,5,0,8,0,0,0,0,0,6,0,0,5,0,0,0,0,5,0,0,0,9,8,0,0,3,0,8,1,0,0,0,9,0,0,0,0,0,6,2,0,0,0,0};
 int convert[2][9] = {0,1,2,3,4,5,6,7,8,0,0,0,3,3,3,6,6,6};
 
 struct game
 {
     int display();
+    int menu();
+    int inputsudo();
     int loopsolve();
     int solve1(); /** NARROW DOWN POSSIBLE NUMBERS **/
     int solve2();/** IF ONLY ONE NUMBER IS POSSIBLE IN AN ELEMENT, ADD IT TO THE SOLUTION **/
@@ -22,26 +24,95 @@ game obj;
 
 int main()
 {
+    obj.menu();
+}
+
+int game::menu()
+{
+    system("cls");
+    system("title -SUDOKU SOLVER-");
+    printf("      -SUDOKU SOLVER-\n\n\n 1. Input Sudoku and solve\n 2. Exit");
+    switch(getch())
+    {
+    case 49:
+        obj.inputsudo();
+        break;
+    case 50:
+        break;
+    default:
+        obj.menu();
+        break;
+    }
+}
+
+int game::inputsudo()
+{
     for(int i=0;i<9;i++)
     {
         for(int j=0;j<9;j++)
         {
+            sudo[i][j] = 0;
             for(int k=0;k<9;k++)
             {
                 solvegrid[i][j][k] = i+1;
             }
         }
     }
-    obj.loopsolve();
+    int input;
+    obj.display();
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            printf("\n\n Enter 0 for blank spaces, press backspace to re-enter previous cell");
+            input = getch();
+            if(input==8)
+            {
+                if(j==0&&i!=0){i=i-1;j=7;}
+                else if(j!=0){j=j-2;}
+            }
+            else if(input<48||input>57&&input!=8){printf("\n\n Incorrect input, returning to menu...(press any key to continue)");getch();obj.menu();}
+            else{sudo[i][j] = input - 48;}
+            obj.display();
+        }
+    }
+    printf("\n\n Input successfully taken, press Y to solve sudoku, or, press N to go back to the main menu.");
+    switch(getch())
+    {
+        case 121: //y
+            obj.loopsolve();
+            break;
+        case 89:
+            obj.loopsolve();
+            break;
+        case 110:
+            obj.menu();
+            break;
+        case 78:
+            obj.menu();
+            break;
+        default:
+            obj.menu();
+            break;
+    }
 }
 
 int game::loopsolve()
 {
+    bool flag;
     obj.solve1();
     obj.solve2();
     obj.display();
-    getch();
-    obj.loopsolve();
+    flag = false;
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            if(sudo[i][j]==0){flag = true;}
+        }
+    }
+    if(flag = true){obj.loopsolve();}
+    else{printf("\n\nSolved! (press any button to return to main menu)");getch();obj.menu();}
 }
 
 int game::solve1()
